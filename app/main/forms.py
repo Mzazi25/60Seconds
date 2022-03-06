feff5513b6eefd0e1df99239f3537473b27428db
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-03-06 20:26:42 +0300

    Updated Forms and added Pitches and Comments classes

---
+++ app/main/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms.widgets import TextArea
+from wtforms.validators import InputRequired
+from wtforms import SelectField,StringField
+
+
+class Pitches(FlaskForm):
+   message = StringField('message', widget=TextArea(), validators=[InputRequired()])
+   category = SelectField('Pitch Category',choices=[('Love','Love'),('Positive','Positive/Inspiring'),('Meme','Memes/Funny'),('Family','Family/Life')])
+class CommentForm(FlaskForm):
+      comment = StringField('comment', widget=TextArea(), validators=[InputRequired()])
