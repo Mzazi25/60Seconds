d3d673c06aaf83957eb9852fb71ab849330a6e37
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-03-06 14:43:55 +0300

    setup db and initialized extensions

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+
+class Config:
+    """
+    General configuration parent class
+    """
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:mzazi25@localhost/watchlist'
+
+class ProdConfig(Config):
+    pass
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:mzazi25@localhost/watchlist_test'
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:Mzazi25@localhost/watchlist'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
+
