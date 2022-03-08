dab55b94b7ff8e58399abf84ee11d250a6efb0ed
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-03-08 10:12:55 +0300

    Updated flask script

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_uploads import UploadSet,configure_uploads,IMAGES
+# from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 
 
@@ -15,7 +15,7 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-photos = UploadSet('photos',IMAGES)
+# photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
 
@@ -31,7 +31,7 @@ def create_app(config_name):
     mail.init_app(app)
 
     # configure UploadSet
-    configure_uploads(app,photos)
+    # configure_uploads(app,photos)
 
     # Registering the blueprint
     from .auth import auth as auth_blueprint
@@ -39,6 +39,5 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
-
 
     return app
