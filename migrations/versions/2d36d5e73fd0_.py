ca21151854e356ce7973cf7faf691f88ada9834f
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-03-10 11:57:31 +0300

    Added files

---
+++ migrations/versions/2d36d5e73fd0_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 2d36d5e73fd0
+Revises: 
+Create Date: 2022-03-10 11:56:44.430366
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2d36d5e73fd0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
